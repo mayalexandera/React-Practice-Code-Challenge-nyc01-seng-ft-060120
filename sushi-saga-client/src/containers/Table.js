83719fc8f9d58a89f717d7b49cc46732fec8ddfd
mayalexandera <maya.blevins@gmail.com>
2020-08-09 15:59:53 -0400

    Done.

--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -4,7 +4,7 @@ const Table = (props) => {
 
   const renderPlates = (array) => {
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
+      return <div key={index} className="empty-plate" style={{ top: -7 * index }}/>
     })
   }
 
@@ -21,7 +21,7 @@ const Table = (props) => {
                and renders an empty plate
                for every element in the array
             */
-            renderPlates([])
+            renderPlates(props.eaten)
           }
         </div>
       </div>
